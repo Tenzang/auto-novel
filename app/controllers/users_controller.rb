2e6fae11d448ff1475da8e1fd2759f0694453159
Loden <loden.gendun@students.mq.edu.au>
2022-04-11 14:29:44 +1000

    initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  before_action :check_for_login
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path # you can redirect wherever you want
+    else
+      render :new # show them the form again so they can try again
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root :to => 'pages#home'
+
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
+
+  resources :users, :only => [:new, :create]
+end
